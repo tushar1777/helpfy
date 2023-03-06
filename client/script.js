d2fee208e8eb49295a2f09499d3dda6cf4f5daa2
tushar-jaiswal-professional <tusharjaiswal.professional@gmail.com>
2023-03-06 11:56:11 +0530

    new update

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('https://helpfy-1.onrender.com', {
+    const response = await fetch('https://helpfy-xa26.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
